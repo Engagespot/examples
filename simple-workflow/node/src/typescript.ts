293bbe592b3d8986343ce4378e13864b632cf6cf
Anand <anandrmedia@gmail.com>
2024-11-07 11:42:19 +0530

    fix(node): added dataRegion to example

--- simple-workflow/node/src/typescript.ts
+++ simple-workflow/node/src/typescript.ts
@@ -4,7 +4,7 @@ const apiKey = 'ENGAGESPOT_API_KEY';
 const apiSecret = 'ENGAGESPOT_API_SECRET';
 
 //Initialize Engagespot object
-const client = EngagespotClient({apiKey, apiSecret});
+const client = EngagespotClient({apiKey, apiSecret, dataRegion: 'eu'});
 
 const result = client.send({
     notification:{
