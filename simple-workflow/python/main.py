85b582c7790852e4dc1af7b3d0bf3542ab927916
Anand <anandrmedia@gmail.com>
2024-11-07 12:46:32 +0530

    fix(python): added data region

--- simple-workflow/python/main.py
+++ simple-workflow/python/main.py
@@ -1,7 +1,7 @@
 from engagespot import Engagespot
 
 # Initialize the client with your API key and secret
-client = Engagespot(api_key="ENGAGESPOT_API_KEY", api_secret="ENGAGESPOT_API_SECRET")
+client = Engagespot(api_key="ENGAGESPOT_API_KEY", api_secret="ENGAGESPOT_API_SECRET", data_region="us")
 
 # Define the payload for the notification
 send_request = {
